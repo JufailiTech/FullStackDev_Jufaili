0ef2920aade89680236dc5bb2bce4099f43b2335
jufailitech <jufailitech@gmail.com>
2023-04-28 23:28:46 +0400

    Statistics is extracted into its own Statistics component, ex. 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,19 @@
 import { useState } from "react";
 
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <>
+    <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {good + bad + neutral}</p>
+      <p>Average: {(good - bad) / (good + bad + neutral)}</p>
+      <p>Positive: {((good / (good + bad + neutral)) * 100).toFixed(13)} %</p>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -12,13 +26,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>Good</button>
       <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
       <button onClick={() => setBad(bad + 1)}>Bad</button>
-      <h2>Statistics</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {good + bad + neutral}</p>
-      <p>Average: {(good - bad) / (good + bad + neutral)}</p>
-      <p>Positive: {((good / (good + bad + neutral)) * 100).toFixed(13)} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };

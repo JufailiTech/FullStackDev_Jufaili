eeb843a4982ae0b2519af9146e386fbfdd8c3bf8
jufailitech <jufailitech@gmail.com>
2023-04-28 10:21:40 +0400

    Added course information, exercise 1.1

--- part1/codeinfo/src/App.js
+++ part1/codeinfo/src/App.js
@@ -1,7 +1,23 @@
-const App = () => (
-  <div>
-    <h1>Hi React!</h1>
-  </div>
-);
+import Header from './Header';
+import Content from './Content';
+import Total from './Total';
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content p1={part1} ex1={exercises1} p2={part2} ex2={exercises2} p3={part3} ex3={exercises3} />
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+    </div>
+  )
+}
 
 export default App;
---
+++ part1/codeinfo/src/Content.js
@@ -0,0 +1,15 @@
+const Content = (props) => (
+  <>
+    <p>
+      {props.p1} {props.ex1}
+    </p>
+    <p>
+      {props.p2} {props.ex2}
+    </p>
+    <p>
+      {props.p3} {props.ex3}
+    </p>
+  </>
+);
+
+export default Content;
